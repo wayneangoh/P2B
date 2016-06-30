7808279592a6d88c680e4c54c6ebae42b9b247dd
Wayne Goh <wayneangoh@gmail.com>
2016-06-30 18:34:57 +0800

    removed cart inventories

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   def show
-    current_order.order_items.destroy_all
+    # current_order.order_items.destroy_all
     @order_items = current_order.order_items
   end
 end
