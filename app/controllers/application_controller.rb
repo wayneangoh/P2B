25c96ed055f185ea64dac6022c873df79ac0b7c7
Wayne Goh <wayneangoh@gmail.com>
2016-06-30 17:03:02 +0800

    added devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
   helper_method :current_order
+
 
   def current_order
     if !session[:order_id].nil?
@@ -9,4 +12,12 @@ class ApplicationController < ActionController::Base
       Order.new
     end
   end
+
+  protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) << :name
+  devise_parameter_sanitizer.for(:account_update) << :name
+end
+
 end
